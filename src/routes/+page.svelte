8f8ec591fffac1419ead2fc80643f1647b05c188
Kevin Miller <scoundrel@gmail.com>
2022-11-05 09:53:54 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<div class="container text-center p-6 my-2">
+
+    <h1 class="text-2xl font-semibold text-primary-100">Fm.Rodeo</h1>
+    <p>
+        Thank you for visiting with us. 
+    </p>
+
+</div>
