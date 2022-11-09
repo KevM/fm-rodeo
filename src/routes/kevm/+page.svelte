9d303b26f1c1dea53267bdfd7ccd5a24491a4d97
Kevin Miller <scoundrel@gmail.com>
2022-11-09 08:05:12 -0600

    Add rel=me to mastodon link

--- src/routes/kevm/+page.svelte
+++ src/routes/kevm/+page.svelte
@@ -10,7 +10,7 @@
 	<section class="flex space-x-4">
       <Avatar class="mr-4" src="/kevm/avatar.jpg" />
       <a class="btn btn-filled-primary" href="https://github.com/kevm" target="_blank" rel="noreferrer">GitHub</a>
-      <a class="btn btn-filled-accent" href="https://toot.community/@kevm/" target="_blank" rel="noreferrer">Mastodon</a>
+      <a rel="me" class="btn btn-filled-accent" href="https://toot.community/@kevm/" target="_blank" rel="noreferrer">Mastodon</a>
       <a class="btn btn-filled-accent" href="https://twitter.com/kevm/" target="_blank" rel="noreferrer">Twitter</a>
       <a class="btn btn-filled-tertiary" href="https://youtube.com/KevinDangerMiller/" target="_blank" rel="noreferrer">YouTube</a>
     </section>
